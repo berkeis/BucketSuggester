bc8b9ecbd41fcd7803a4d8a9e67bf86f70362989
Berke Isin Can <berke.can@defia-soft.com>
2019-05-08 14:28:50 +0300

    changed port definition in app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/albums', album);
 
-let port = 3000;
+var port = Number(process.env.PORT || 3000);
 
 app.listen(port, () => {
     console.log('Server is up and running on port number ' + port);
