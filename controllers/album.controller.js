5708e23c6c65c44f005558fc726a78b0d5def089
gsoykan <gsoykan@sabanciuniv.edu>
2019-05-10 21:27:34 +0300

    minorite

--- controllers/album.controller.js
+++ controllers/album.controller.js
@@ -1,51 +1,63 @@
 var Album = require('../models/album.model');
 
 
+// @ts-ignore
 exports.landing = function (req, res) {
     res.send('It is alive!');
 };
 
 exports.album_create = function (req, res) {
 
-	var album = new Album(
+    var album = new Album(
         {
-            name: req.body.name,
+            name: req.body.name2,
             length: req.body.length
         }
     );
 
     album.save(function (err) {
         if (err) {
-            return next(err);
+            // @ts-ignore
+            return res.send(err);
         }
         res.send('Album Created successfully')
     })
 };
 
+// @ts-ignore
 exports.album_allRecords = function (req, res) {
-	Album.find({}, function (err, results) {
+    Album.find({}, function (err, results) {
+        // @ts-ignore
         if (err) return next(err);
-		res.send(results);
-	});
+        res.send(results);
+    });
 };
 
 exports.album_details = function (req, res) {
-	Album.findById(req.params.id, function (err, album) {
+    Album.findById(req.params.id, function (err, album) {
+        // @ts-ignore
         if (err) return next(err);
         res.send(album);
     });
 };
 
 exports.album_update = function (req, res) {
-    Album.findByIdAndUpdate(req.params.id, {$set: req.body}, function (err, album) {
+    // @ts-ignore
+    Album.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, album) {
+        // @ts-ignore
         if (err) return next(err);
         res.send('Album udpated.');
     });
 };
 
-exports.album_delete = function (req, res) {
-	Album.findByIdAndRemove(req.params.id, function (err) {
-		if (err) return next(err);
-		res.send('Deleted successfully!');
-	})
+const album_delete1 = (req, res) => {
+    Album.findByIdAndRemove(req.params.id, function (err) {
+        // @ts-ignore
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
 };
+
+module.exports = {
+    album_delete: album_delete1
+}
